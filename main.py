dec1e62a6901e32e9de0546e699bebbfeebe8552
Tobel <tobiasz.salik@gmail.com>
2020-11-02 18:24:15 +0100

    bombEgg

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ lives = 3
 width = 800
 height = 600
 window = pyglet.window.Window(width, height)
+
 space = pymunk.Space()
 
 
@@ -16,11 +17,20 @@ def removeEgg(eggShape):
     eggs.remove(eggShape)
     space.remove(eggShape.body, eggShape)
 
+
 def catchHealerEgg(arbiter, space, data):
     global lives
     lives += 1
     removeEgg(arbiter.shapes[0])
     return False
+
+
+def catchBombEgg(arbiter, space, data):
+    global lives
+    lives -= 1
+    removeEgg(arbiter.shapes[0])
+    return False
+
 
 def catchRegularEgg(arbiter, space, data):
     global points
@@ -28,34 +38,47 @@ def catchRegularEgg(arbiter, space, data):
     removeEgg(arbiter.shapes[0])
     return False
 
+
 collisionTypes = {
     "basket": 0,
     "regularEgg": 1,
-    "healerEgg": 2
+    "healerEgg": 2,
+    "bombEgg": 3
 }
 
 eggTypes = [
     ("regularEgg", (50, 255, 50, 255), 70),
-    ("healerEgg", (50, 50, 255, 255), 5)
+    ("healerEgg", (50, 50, 255, 255), 5),
+    ("bombEgg", (50, 50, 100, 255), 5)
 ]
-eggInBasketHandler = space.add_collision_handler(collisionTypes["regularEgg"], collisionTypes["basket"])
+
+
+eggInBasketHandler = space.add_collision_handler(
+    collisionTypes["regularEgg"], collisionTypes["basket"])
 eggInBasketHandler.begin = catchRegularEgg
 
-eggInBasketHandler = space.add_collision_handler(collisionTypes["healerEgg"], collisionTypes["basket"])
+eggInBasketHandler = space.add_collision_handler(
+    collisionTypes["healerEgg"], collisionTypes["basket"])
 eggInBasketHandler.begin = catchHealerEgg
+
+eggInBasketHandler = space.add_collision_handler(
+    collisionTypes["bombEgg"], collisionTypes["basket"])
+eggInBasketHandler.begin = catchBombEgg
 
 movingLeft = False
 movingRight = False
 maxSpeed = 500
 
 eggs = []
+
+
 def generateEgg():
     def randomEggType():
-        return random.choices(eggTypes, weights=[type[2] for type in eggTypes], k = 1)[0]  
+        return random.choices(eggTypes, weights=[type[2] for type in eggTypes], k=1)[0]
     radius = 20
-    eggBody = pymunk.Body(1, 1)
+    eggBody = pymunk.Body(1, pymunk.inf)
     eggBody.position = random.randint(radius, width-radius), height + radius
-    eggBody.velocity = 0, -random.randint(100,300)
+    eggBody.velocity = 0, -random.randint(100, 300)
     eggType, eggColor, _ = randomEggType()
     eggShape = pymunk.Circle(eggBody, radius)
     eggShape.color = eggColor
@@ -63,17 +86,21 @@ def generateEgg():
     eggs.append(eggShape)
     space.add(eggBody, eggShape)
 
+
 def createBasket():
     basketYPos = 20
     basketWidth = 100
     basketThickness = 10
     basketBody = pymunk.Body(1, pymunk.inf)
     basketBody.position = width // 2, basketYPos
-    basketShape = pymunk.shapes.Segment(basketBody, (-basketWidth//2, 0), (basketWidth//2, 0), basketThickness)
+    basketShape = pymunk.shapes.Segment(
+        basketBody, (-basketWidth//2, 0), (basketWidth//2, 0), basketThickness)
     basketShape.collision_type = collisionTypes["basket"]
-    basketConstraint = pymunk.GrooveJoint(space.static_body, basketBody, (basketWidth // 1.5, basketYPos), (width - basketWidth//1.5, basketYPos), (0,0))
+    basketConstraint = pymunk.GrooveJoint(space.static_body, basketBody, (
+        basketWidth // 1.5, basketYPos), (width - basketWidth//1.5, basketYPos), (0, 0))
     space.add(basketBody, basketShape, basketConstraint)
     return basketBody
+
 
 def updateVelocity():
     global basketBody
@@ -84,6 +111,16 @@ def updateVelocity():
         vel += maxSpeed
     basketBody.velocity = vel, 0
 
+
+def removeFallenEggs():
+    for eggShape in eggs:
+        if eggShape.body.position[1] < eggShape.radius:
+            if eggShape.collision_type != collisionTypes["bombEgg"]:
+                global lives
+                lives -= 1
+            removeEgg(eggShape)
+
+
 @window.event
 def on_key_press(symbol, modifiers):
     global movingLeft, movingRight
@@ -92,6 +129,7 @@ def on_key_press(symbol, modifiers):
     if symbol == key.RIGHT:
         movingRight = True
     updateVelocity()
+
 
 @window.event
 def on_key_release(symbol, modifiers):
@@ -105,27 +143,33 @@ def on_key_release(symbol, modifiers):
 
 timeToGenerateEgg = 2
 level = 3
+
+
 def update(dt):
     global timeToGenerateEgg
     timeToGenerateEgg -= dt
     if timeToGenerateEgg < 0:
         timeToGenerateEgg = random.uniform(level-0.5, level+0.5)
         generateEgg()
+    removeFallenEggs()
     space.step(dt)
 
 
-
 pointsLabel = pyglet.text.Label('Points',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=width - 120, y=height - 50,
-                          anchor_x='center', anchor_y='center')
+                                font_name='Times New Roman',
+                                font_size=36,
+                                x=width - 120, y=height - 50,
+                                anchor_x='center', anchor_y='center')
 livesLabel = pyglet.text.Label('Lives',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=width - 120, y=height - 90,
-                          anchor_x='center', anchor_y='center')
+                               font_name='Times New Roman',
+                               font_size=36,
+                               x=width - 120, y=height - 90,
+                               anchor_x='center', anchor_y='center')
+
+
 options = pymunk.pyglet_util.DrawOptions()
+
+
 @window.event
 def on_draw():
     window.clear()
@@ -134,7 +178,8 @@ def on_draw():
     pointsLabel.draw()
     livesLabel.draw()
     space.debug_draw(options)
-    
+
+
 pyglet.clock.schedule_interval(update, 1/60)
 
 basketBody = createBasket()
